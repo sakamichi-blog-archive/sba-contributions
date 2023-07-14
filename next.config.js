f411ddddd7d4f07428c72ce621fe889d2ac108da
me@sakamichi.co <me@sakamichi.co>
2023-07-14 14:38:26 +0900

    :wrench: next.config.js: Migrate to output: export

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 })
 
 module.exports = withBundleAnalyzer({
+  output: "export",
   sassOptions: {
     includePaths: [
       path.join(__dirname, "src/styles")
